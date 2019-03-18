4339e77887d454d7b8645fe4c5fe3e4dbf0f1303
Matt Williams <matt.seongjun@gmail.com>
2019-03-18 21:12:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string)
 }
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too"
+  }
+  else if (string.shout() === string) {
+    return "YES INDEED!"
+  }
+  else if (string.whisper() === string) {
+    return "I can't hear you!"
+  }
+}
